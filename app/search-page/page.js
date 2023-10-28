91bf431f5994aea1d9d20ded9e3ab3a7af82a151
sushmullur <mullur.sushruth@gmail.com>
2023-10-28 07:49:46 -0700

    Auth and more colors

--- app/search-page/page.js
+++ app/search-page/page.js
@@ -1,11 +1,20 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { useUser } from '@auth0/nextjs-auth0/client';
 import Ribbon from "@/components/ribbon";
 import ExportNotion from "@/components/exportNotion";
 
 export default function SearchPage() {
   const [search, setSearch] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const { user } = useUser();
+  if (!user) { 
+    return (
+      <>
+        <p>Unauthorized</p>
+      </>
+    )
+  }
   const [data, setData] = useState();
   const handleSearch = async (e) => {
     console.log(search)
@@ -35,23 +44,23 @@ export default function SearchPage() {
   } 
 
   return (
-    <main class="h-screen bg-gray-800">
+    <main className="h-screen bg-gradient-to-b from-gray-800 via-blue-400 to-blue-800 text-white">
         <Ribbon />
-        <div class="flex mt-5 justify-center">
-            <div class="flex flex-col bg-gray-700 rounded-lg w-70vw h-48 p-8 space-y-5">
-                <p class="justify-self-center self-center font text-xl">Search your study topic!</p>
-                <div class="flex justify-center mt-10 space-x-2">
+        <div className="flex mt-5 justify-center">
+            <div className="flex flex-col bg-gray-700 rounded-lg w-70vw h-48 p-8 space-y-5">
+                <p className="justify-self-center self-center font text-xl">Search your study topic!</p>
+                <div className="flex justify-center mt-10 space-x-2">
                     <input
                         type="text"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-750 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-750 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         onChange={(e) => setSearch(e.target.value)}
                     />
                     <button 
-                        class="text-black bg-slate-200 hover:bg-slate-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-200 dark:hover:bg-slate-300 dark:focus:ring-blue-800"
+                        className="text-black bg-slate-200 hover:bg-slate-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-200 dark:hover:bg-slate-300 dark:focus:ring-blue-800"
                         onClick={handleSearch}
                     >Search</button>
                     </div>
-                    <div class="flex justify-center mt-5">
+                    <div className="flex justify-center mt-5">
                     {isLoading ? (
                     <p>Loading...</p>
                     ) : (
--- components/ribbon.js
+++ components/ribbon.js
@@ -1,21 +1,34 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
+import Link from "next/link";
 
 export default function Ribbon() {
-    const router = useRouter();
-    const handleRouting = (e) => {
-        e.preventDefault();
-        router.push('/');
-    }
-
-    return (
-        <div class="flex justify-between items-center mr-8">
-            <div class="flex items-center space-x-4">
-                <svg class="ml-5" xmlns="http://www.w3.org/2000/svg" width="98" height="98" viewBox="0 0 24 24"><path fill="red" d="M12 2a2 2 0 0 1 2 2c0 .74-.4 1.39-1 1.73V7h1a7 7 0 0 1 7 7h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1a7 7 0 0 1 7-7h1V5.73c-.6-.34-1-.99-1-1.73a2 2 0 0 1 2-2M7.5 13A2.5 2.5 0 0 0 5 15.5A2.5 2.5 0 0 0 7.5 18a2.5 2.5 0 0 0 2.5-2.5A2.5 2.5 0 0 0 7.5 13m9 0a2.5 2.5 0 0 0-2.5 2.5a2.5 2.5 0 0 0 2.5 2.5a2.5 2.5 0 0 0 2.5-2.5a2.5 2.5 0 0 0-2.5-2.5Z"/></svg>
-                <p class="text-white font-serif font-bold text-6xl text-stroke">study.yt</p>
-            </div>
-            <button class="text-white font-serif" onClick={handleRouting}>Log Out</button>
-        </div>
-    )
+  return (
+    <div className="flex justify-between items-center mr-8">
+      <div className="flex items-center space-x-4">
+        <svg
+          className="ml-5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="98"
+          height="98"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="red"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path
+            d="M12 2a2 2 0 0 1 2 2c0 .74-.4 1.39-1 1.73V7h1a7 7 0 0 1 7 7h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1a7 7 0 0 1 7-7h1V5.73c-.6-.34-1-.99-1-1.73a2 2 0 0 1 2-2M7.5 13A2.5 2.5 0 0 0 5 15.5A2.5 2.5 0 0 0 7.5 18a2.5 2.5 0 0 0 2.5-2.5A2.5 2.5 0 0 0 7.5 13m9 0a2.5 2.5 0 0 0-2.5 2.5a2.5 2.5 0 0 0 2.5 2.5a2.5 2.5 0 0 0 2.5-2.5a2.5 2.5 0 0 0-2.5-2.5Z"
+          />
+        </svg>
+        <p className="text-white font-serif font-bold text-6xl text-stroke">study.yt</p>
+      </div>
+      <Link href="/api/auth/logout">
+        <button className="font-bold font-serif bg-gradient-to-r from-blue-500 to-blue-600 text-white text-3xl rounded-full w-60 h-16 px-6 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 transform hover:scale-105 transition-transform">
+          Log Out
+        </button>
+      </Link>
+    </div>
+  );
 }
