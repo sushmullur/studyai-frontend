71e47b590ac1e247aa417d3c5705ccc9ab382435
Dylan Hopkins <dylanhopkins@berkeley.edu>
2023-10-28 00:41:27 -0700

    fetch api from search button

--- app/page.js
+++ app/page.js
@@ -1,26 +1,7 @@
-import Image from 'next/image'
+import SearchPage from './searchPage'
 
 export default function Home() {
-
   return (
-    <main style={{backgroundColor: 'white', height: '100vw',
-    padding: '20vw'}}>
-      <div>
-        <input
-          type="text"
-          style={{
-            color: 'black',
-            border: '1px solid black',
-            width: '30vw',
-          }}
-        />
-        <button 
-          style={{
-            backgroundColor: 'blue',
-            color: 'white',
-          }}
-        >Search</button>
-      </div>
-    </main>
+    <SearchPage />
   )
 }
