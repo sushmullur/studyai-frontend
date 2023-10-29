952fc338ef1e5a59892aeebd41cbfd50baa5fe5f
Dylan Hopkins <dylanhopkins@berkeley.edu>
2023-10-28 19:02:10 -0700

    made links clickable in table

--- components/table.js
+++ components/table.js
@@ -6,9 +6,9 @@ export default function SQLTable(props) {
     console.log("props data",props.data)
     if (props.data) {
         return (
-            <table className="text-black">
+            <table>
                 <thead>
-                <tr>
+                <tr className="text-black">
                     <th className="border border-black p-2">Query</th>
                     <th className="border border-black p-2">Download Link</th>
                     <th className="border border-black p-2">Export</th>
@@ -17,8 +17,8 @@ export default function SQLTable(props) {
                 <tbody>
                 {props.data.map((row) => (
                     <tr key={row.user_id}>
-                    <td className="border border-black p-2">{row.query_text}</td>
-                    <td className="border border-black p-2">{row.fileurl}</td>
+                    <td className="text-black border border-black p-2">{row.query_text}</td>
+                    <td className="text-cyan-500 underline-offset-1 underline border border-black p-2"><a href={row.fileurl}>{row.fileurl}</a></td>
 
                     <div className="border border-black p-2">
                       <ExportNotion data={row}/>
